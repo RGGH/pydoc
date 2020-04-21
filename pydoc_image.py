c59711c61d3115e1a5f4727fab7486b5d065cc24
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-04-21 12:19:15 +0100

    Create pydoc_image.py

---
+++ pydoc_image.py
@@ -0,0 +1,23 @@
+
+from docxtpl import DocxTemplate
+import os
+
+image_dir = 'templates/images/'
+files = files = [ f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(imag$
+
+
+tpl = DocxTemplate('templates/replace_picture_tpl.docx')
+
+for file in files:
+
+        DEST_FILE = f'output/auto_{file.rsplit(".",1)[0]}.docx'
+
+        context = {}
+
+
+        old_im = "fruit_logo.png"
+        new_im = f"templates/images/{file}"
+
+        tpl.replace_pic(old_im, new_im)
+        tpl.render(context)
+        tpl.save(DEST_FILE)
