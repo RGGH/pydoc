2807c692984e11d75cf1b9baf371622b9f47ba8a
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-06-29 15:54:49 +0100

    Altered to use value from column for filename
    See : https://youtu.be/_BVr0h3rlGI on YouTube

--- conditional/pdoc-csv.py
+++ conditional/pdoc-csv.py
@@ -13,14 +13,14 @@ from docxtpl import DocxTemplate
 # Source CSV - column names that must match the *** that are {{***}} inside "template.docx"
 csvfn = "cnw.csv"
 
-def mkw(n):
+def mkw(n,fname):
     tpl = DocxTemplate("template.docx") # In same directory
     df = pd.read_csv(csvfn)
     df_to_doct = df.to_dict() # dataframe -> dict for the template render
     x = df.to_dict(orient='records')
     context = x
     tpl.render(context[n])
-    tpl.save("%s.docx" %str(n+1))
+    tpl.save("%s.docx" %fname)
 
     # Wait a random time - increase to (60,180) for real production run.
     wait = time.sleep(random.randint(1,2))
@@ -28,10 +28,15 @@ def mkw(n):
 #-------------------Main---------------------#
 
 df2 = len(pd.read_csv(csvfn))
+
 print ("There will be ", df2, "files")
 
 for i in range(0,df2):
-    print("Making file: ",f"{i}," ,"..Please Wait...")
-    mkw(i)
+    df = pd.read_csv(csvfn)
+    columnsData = df.loc[ : , 'title' ]
+    fname = columnsData[i]
+    print("Making file: ",f"{fname}," ,"..Please Wait...")
+    mkw(i,fname)
 
 print("Done! - Now check your files")
+
